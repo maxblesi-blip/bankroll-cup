a4bf76e871d1308df7b460bfc9455ed9a3613032
maxblesi-blip <maxblesi@gmail.com>
2025-10-26 16:14:51 +0100

    Update Discord role ID to 1432024313938051164

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,7 +4,7 @@
  * Discord Role IDs für Zugriffskontrolle
  */
 export const DISCORD_ROLES = {
-  BANKROLL_CUP_PARTICIPANT: "1430253515036557506",
+  BANKROLL_CUP_PARTICIPANT: "1432024313938051164",
 } as const;
 
 export type DiscordRole = typeof DISCORD_ROLES[keyof typeof DISCORD_ROLES];
